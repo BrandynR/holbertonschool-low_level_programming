e7866102d67016fe91a331a68495aff51a928ecb
BrandynR <BrandynR@github.com>
2019-06-19 20:55:03 +0000

    Forgot the space after return

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
 *main - Enrty point
 *
-*Return: Always 0 9Sucess)
+*Return: Always 0 (Sucess)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
