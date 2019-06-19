e3e58b72e153e2650ba387a8a2d364de59544d41
BrandynR <BrandynR@github.com>
2019-06-19 21:26:35 +0000

    Added 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main ()
+{
+	printf("Size of a char: %c");
+	printf("Size of an int" %i");
+	printf("Size of a long int: %l");
+	printf("Size of a long long int: %L");
+	printf("Size of a float: %f");
+return (0);
+}
