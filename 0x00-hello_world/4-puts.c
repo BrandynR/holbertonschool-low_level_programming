0fee806b54db01fd89d3d96d13e544c1d16a93f6
BrandynR <BrandynR@github.com>
2019-06-19 20:08:39 +0000

    corrected typos

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
-#include <stdio>
-
+#include <stdio.h>
 /**
 *main - Enrty point
 *
 *Return: Always 0 9Sucess)
 */
-
 int main(void) 
 {
-	puts(/"Programming is like building a multilingual puzzle\n)
-
+	puts("Programming is like building a multilingual puzzle\n);
 	return(0);
 }
