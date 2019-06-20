269145e6529385c90cdd8a3ae122e79a0bfd76aa
BrandynR <BrandynR@github.com>
2019-06-20 22:28:30 +0000

    Added task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing numbers 0 to 10
+ *
+ *return: numbers listed in order
+ */
+
+int main(void)
+{
+int x;
+
+for (x = '0'; x <= '9'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
