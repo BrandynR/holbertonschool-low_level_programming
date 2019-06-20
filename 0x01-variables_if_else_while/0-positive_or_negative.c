3a8b876b9e22fc2aab6a4a245bb4b24b05b64adc
BrandynR <BrandynR@github.com>
2019-06-20 19:46:38 +0000

    Removed spaces

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
  * main - Inputs random number
  *
@@ -8,15 +8,14 @@
  */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive\n", n);
 if (n < 0)
 printf("%d is negative\n", n);
 if
-printf("%d is zero\n", n); }
+printf("%d is zero\n", n);
 return (0);
 }
