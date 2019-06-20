194ca2f44be4a578e27cd250e73c73d8a8e5c182
BrandynR <BrandynR@github.com>
2019-06-20 20:29:15 +0000

    Added 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Changing the last number at random
+ *
+ *Return: Whether it is >,<, or = 0
+ */
+for (int x = 'A'; x <= 'Z'; x++)
+{
+int lower_x = tolower(x);
+putchar(lower_x);
+}
