ba7a606c99bb2146c39cd9f52dd55454dae64be1
BrandynR <BrandynR@github.com>
2019-06-20 21:56:57 +0000

    Added 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Letters of the alphabet
+ *
+ * Return: alpahbet withour q and e
+ */
+
+int main(void)
+{
+int x;
+
+for (x = 'abcdfghijklmnoprstuvwxyz')
+putchar(x);
+putchar('\n');
+return (0);
+}
