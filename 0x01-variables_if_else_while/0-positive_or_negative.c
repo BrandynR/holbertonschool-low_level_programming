7575078abad727734747db95e1a97f4e401e14d8
BrandynR <BrandynR@github.com>
2019-06-20 20:05:00 +0000

    Deleted whitespace

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,9 +13,9 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive\n", n);
-if (n < 0)
+else if (n < 0)
 printf("%d is negative\n", n);
-if
+else
 printf("%d is zero\n", n);
 return (0);
 }
