e43c999c26270ad950548dc768fb2e0bba1a25b7
BrandynR <BrandynR@github.com>
2019-06-21 22:12:14 +0000

    Added comments

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ *main - printing to stderr
+ *
+ *Return: print a line without using printf or puts
+ */
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+
+return (1);
 }
