72dfada679ea37e1fcf5a1be1bc9d1cdc0c30af6
BrandynR <BrandynR@github.com>
2019-06-26 00:38:02 +0000

    Removed extra for loop

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,18 @@
 #include "holberton.h"
 
 /**
- *int _islower(int c) - checks for lower case letter
- *
+ *int _islower - checks for lower case letter
+ *@c: represents a lowercase letter
  *Return: 0 for uppercase 1 for lowercase
  */
 
 int _islower(int c);
-{
-	int c;
 
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else if (c >= 'A' && c <= 'Z')
-	{
-		return (0);
-	}
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+}
+{
+else
+return (0);
 }
