f70f58bfe4f326239da5aae78bd48d40ead70ae0
BrandynR <BrandynR@github.com>
2019-06-26 00:45:07 +0000

    Removed extra semi-colon

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,13 +6,12 @@
  *Return: 0 for uppercase 1 for lowercase
  */
 
-int _islower(int c);
+int _islower(int c)
 
 {
-if (c >= 'a' && c <= 'z')
+if ((c >= 'a') && (c <= 'z'))
 return (1);
-}
-{
+
 else
 return (0);
 }
