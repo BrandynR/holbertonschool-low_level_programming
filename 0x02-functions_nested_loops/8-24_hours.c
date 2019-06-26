a804eeb6825c3c5202e6535b39cd0fb87e4a3ff5
BrandynR <BrandynR@github.com>
2019-06-26 03:28:39 +0000

    Fixed bracket positions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,16 +10,30 @@ void jack_bauer(void)
 {
 	int w, x, y, z;
 
-	for (w  = '0'; w <= '2'; w++)
-		for (x = '0'; x <= '3'; x++)
-			for (y = '0'; y <= '5'; y++)
-				for (z = '0'; z <= '9'; z++)
+	for (w  = 0; w <= 2; w++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			for (y = 0; y <= 5; y++)
+			{
+				for (z = 0; z <= 9; z++)
 				{
-					_putchar(w);
-					_putchar(x);
+					if (w == 2 && x > 3)
+					{
+						break;
+					}
+					else
+					{
+					_putchar(w + '0');
+					_putchar(x + '0');
 					_putchar(':');
-					_putchar(y);
-					_putchar(z);
+					_putchar(y + '0');
+					_putchar(z + '0');
 					_putchar('\n');
+					}
 				}
+			}
+		}
+
+	}
 }
