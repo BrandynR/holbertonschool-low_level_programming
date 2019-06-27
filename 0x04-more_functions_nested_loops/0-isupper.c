5da268b354692836580d1646ad672dcfd31805a6
BrandynR <BrandynR@github.com>
2019-06-27 18:13:45 +0000

    Added 2 and 3

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,19 @@
 /**
  *_isupper - Checks for upper case letter
  *@c: represents a letter
- *Return: 1 c is uppercase, 0 if lowercase
+ *Return: 1 if c is uppercase, 0 if lowercase
  */
 
 int _isupper(int c)
 
 {
 	if ((c >= 'A') && (c <= 'Z'))
-	return (1);
+	{
+		return (1);
+	}
 
 	else
-	return (0);
+	{
+		return (0);
+	}
 }
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ *@a: represents one integer
+ *@b: represents one integer
+ */
+
+int mul(int a, int b)
+{
+for (a * b)
+return (0);
+}
