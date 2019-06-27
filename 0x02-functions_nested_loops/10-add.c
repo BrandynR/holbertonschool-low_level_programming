c0dc5563561e76295ccc31f24ad1fdd8edfee57d
BrandynR <BrandynR@github.com>
2019-06-27 00:17:35 +0000

    Added task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *add - Add two integers together
+ *@x: represents a number
+ *@y: represents a number
+ *Return: Always 0
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
