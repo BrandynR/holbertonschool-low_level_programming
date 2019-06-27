afde56cd9124ce7e0876950c34c98dbbca9a1781
BrandynR <BrandynR@github.com>
2019-06-27 17:47:37 +0000

    Added 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,17 @@
-#include "header.h"
+#include "holberton.h"
 
 /**
  *_isupper - Checks for upper case letter
  *@c: represents a letter
- *Result: Returns 1 c is uppercase, 0 if lowercase
+ *Return: 1 c is uppercase, 0 if lowercase
  */
 
-int _isupper(int c);
+int _isupper(int c)
+
 {
-	if (n = 'A'; n <= 'Z')
-		return (1);
+	if ((c >= 'A') && (c <= 'Z'))
+	return (1);
+
 	else
-		return (0);
+	return (0);
 }
