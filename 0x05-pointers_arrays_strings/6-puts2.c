7a3a1a7207d782fb7d1e301b82714ff01df667cf
BrandynR <BrandynR@github.com>
2019-07-03 01:29:53 +0000

    Nested my if statement

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,11 @@ void puts2(char *str)
 int len;
 
 for (len = 0; str[len] != '\0'; len++)
-
-if (len / 2)
-_putchar(len);
-
+{
+if (len % 2 == 0)
+{
+	_putchar(str[len]);
+}
+}
 _putchar('\n');
 }
