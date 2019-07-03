a3e28c3400ace6ca3d44447d4a656c06a679c155
BrandynR <BrandynR@github.com>
2019-07-03 01:00:47 +0000

    Added 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ *rev_string - reverses a string
+ *@s: string
+ *Return: void
+ */
+
+void rev_string(char *s)
+{
+int len = 0;
+
+while (s[len] != '\0')
+{
+*start = 0;
+*end = len - 1;
+temp = *end;
+*end = *start;
+*start = temp;
+}
+for (i = 0; i <= len / 2; i++)
+{
+start++;
+end--;
+}
+}
