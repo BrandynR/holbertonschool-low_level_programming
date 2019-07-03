ec477f9fc755379ed376aa8d7ef9b0087e5189f9
BrandynR <BrandynR@github.com>
2019-07-03 01:21:44 +0000

    Added task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *puts2 - prints 1 out of 2 characters
+ *@str: string
+ *Return: void
+ */
+
+void puts2(char *str)
+{
+int len;
+
+for (len = 0; str[len] != '\0'; len++)
+
+if (len / 2)
+_putchar(len);
+
+_putchar('\n');
+}
