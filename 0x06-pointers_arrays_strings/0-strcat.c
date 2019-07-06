7bfc31c1eded6216431878decca062c66338f717
BrandynR <BrandynR@github.com>
2019-07-06 17:10:58 +0000

    Added tak 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,17 @@
-#inlcude "holberton.h"
+#include "holberton.h"
 
 /**
  *_strcat - concats two strings
- *
+ *@dest: s2
+ *@src: s1
  *Return: a pointer to the resulting string
  */
 
 char *_strcat(char *dest, char *src)
-	char string1[], s;
-	int i = 0, j = 0;
-
-while (string1[i] != '\0')
 {
-	dest[i] = string1[i];
-	i++;
-}
+while(*(++src));
 
-while (src[j] != '\0')
-{
-	dest[i] = src[j];
-		i++;
-		j++;
+while((*(src++) = *(dest++)));
+
+return (dest);
 }
-		dest[i] = '\0';
