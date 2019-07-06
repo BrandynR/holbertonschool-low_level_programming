d4ece9b52821faef03b3cf2072ceae6d71478275
BrandynR <BrandynR@github.com>
2019-07-06 18:57:51 +0000

    Added task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,20 @@
 
 /**
  *_strncpy - function that copies a string
- *
- */Return:
+ *@dest: copy of string
+ *@src: original string
+ *@n: variable
+ *Return: copy of orignal string
+ */
 
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
+{
+int  i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[i] = src[i];
+for ( ; i < n; i++)
+	dest[i] = '\0';
+
+return (dest);
+}
