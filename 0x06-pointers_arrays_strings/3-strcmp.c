5e737b9cd337bf07c697a2b697a334f6ad883510
BrandynR <BrandynR@github.com>
2019-07-06 19:22:02 +0000

    Added 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *_strcmp - funtion to compare two strings
+ *@s1: string 1
+ *@s2: string 2
+ *Return: comparison
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0' || *s2 == '\0')
+			break;
+
+		s1++;
+		s2++;
+	}
+	if (*s1 == '\0' && *s2 == '\0')
+		return (0);
+	else
+		return (-1);
+}
