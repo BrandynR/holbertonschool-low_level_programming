b0f8110200065fff2ee92420887c927b47be4245
BrandynR <BrandynR@github.com>
2019-07-07 17:50:37 +0000

    Added task 8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *rot13 - converts a string to rot13 encryption
+ *@s: string to encrypt
+ *Return: encrypted string
+ */
+
+char *rot13(char *)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+		{
+			s[i] = (s[i] + 13);
+		}
+		else
+			while ((s[i] >= 'n' && s[i] <= 'z') ||
+			       (s[i] >= 'N' && s[i] <= 'Z'))
+			{
+				s[i] = (s[i] - 13);
+			}
+		i++;
+	}
+	return (s)
+}
