4c4bf5aa388373dbdc1ad901d16b84cd36d9a898
BrandynR <BrandynR@github.com>
2019-07-07 18:12:42 +0000

    Fixed output error

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-while (*src)
-{
-	src++;
-}
-while (*dest)
-{
-	*src = *dest;
-	src++;
-	dest++;
-}
-*src = '\0';
-return (dest);
+	int x, y;
+
+	for (x = 0; dest[x] != '\0'; x++)
+
+	y = 0;
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	x++;
+	dest[x] = '\0';
+	return (dest);
 }
