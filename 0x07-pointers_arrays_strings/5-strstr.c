3f1d6de3d1f587426eec997535bc2bd5e594de3e
BrandynR <BrandynR@github.com>
2019-07-09 20:53:15 +0000

    Added 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *_strstr - locates a substring
+ *@haystack: string to be searched
+ *@needle: substring to search for
+ *Return: pointer to beginning, or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	if (*haystack == *needle)
+	{
+		return (haystack);
+	}
+	return (0);
+}
