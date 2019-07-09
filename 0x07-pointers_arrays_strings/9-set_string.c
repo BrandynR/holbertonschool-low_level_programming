12eb042a0228c7f8d084d4880de570c8850ddce9
BrandynR <BrandynR@github.com>
2019-07-09 21:13:32 +0000

    Removed extra *

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -2,11 +2,12 @@
 
 /**
  *set_string - sets value of pointer to a char
- *
+ *@s: pointer to a pointer
+ *@to: char
  *Return: void
  */
 
 void set_string(char **s, char *to)
 {
-        *s = to;
+*s = to;
 }
