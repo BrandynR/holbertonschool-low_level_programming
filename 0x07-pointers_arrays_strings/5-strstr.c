8e312865945c7c911e214ac61e76215e1a1e804f
BrandynR <BrandynR@github.com>
2019-07-10 01:31:25 +0000

    Fixed output

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,28 +1,45 @@
 #include "holberton.h"
 
 /**
- *_strstr - locates a substring
+ *_strncmp - compares two strings
+ *@s1: string 1
+ *@s2: string 2
+ *@b: unsigned int
+ *Return: pointer to beginning, or NULL
+ */
+
+int _strncmp(char *s1, char *s2, unsigned int b)
+{
+	unsigned int j;
+
+	for (j = 0; j <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; j++)
+	{
+		s1++;
+		s2++;
+	}
+	return (j - b);
+}
+
+/**
+ *_strstr - locates a sub string
  *@haystack: string to be searched
  *@needle: substring to search for
- *Return: pointer to beginning, or NULL
+ *Return: haystack, or 0
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	if (*needle == '\0')
-		return (haystack);
+	unsigned int len;
+
+	len = 0;
+
+	while (needle[len] != '\0')
+		len++;
 	while (*haystack != '\0')
 	{
-		if (*haystack == *needle)
-		{
+		if (_strncmp(haystack, needle, len) == 0)
 			return (haystack);
-		}
 		haystack++;
 	}
-	if (*haystack != *needle)
-	{
-		return (haystack);
-	}
-	haystack++;
 	return (0);
 }
