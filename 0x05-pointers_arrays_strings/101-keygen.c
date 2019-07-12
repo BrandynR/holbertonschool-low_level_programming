8deb916c9cfc2ba1a5b6833b4d077327dbfac10e
BrandynR <BrandynR@github.com>
2019-07-12 00:05:02 +0000

    Added 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,33 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/**
- * main - generate random password
+/*
  *
  * Return:0;
  */
 
-int main(void)
-{
-	char pass[100];
-	int ran, num, i;
-
-	num = 0;
-	i = 0;
-	srand(time(NULL));
-	while (num < 2645)
-	{
-		ran = random() % 127;
-		if (ran > 32)
-		{
-			pass[i++] = randNum;
-			num += ran;
-		}
-	}
-	pass[i++] = (2772 - num);
-	pass[i] = '\0';
-	printf("%s", pass);
-
-	return (0);
-}
