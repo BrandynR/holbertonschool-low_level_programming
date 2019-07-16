800c633e0d6ecf89aa383d7cde59f3dca60edd51
BrandynR <BrandynR@github.com>
2019-07-16 21:46:30 +0000

    Corrected task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,42 +1,39 @@
-#include <stdio.h>
+#include "holberton.h"
 #include <stdlib.h>
 
 /**
- *str_concat - concatenates two strings
+ * str_concat - concatenate two strings
  * @s1: string 1
  * @s2: string 2
- * Return: pointer to string
+ * Return: pointer to string, or NULL
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *string;
-	char *empty;
-	int i, j, k;
+	char *a;
+	int i, j, c, d;
 
-	empty = "";
 	if (s1 == NULL)
-		s1 = empty;
+		s1 = "";
 	if (s2 == NULL)
-		s2 = empty;
+		s2 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
-	j++;
 
-	string = malloc((i + j) * sizeof(*str));
-	if (string == NULL)
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (a == NULL)
 		return (NULL);
-	for (k = 0; s1[k] != '\0'; k++)
-		str[k] = s1[k];
-	j = 0;
-	while (s2[j] != '\0')
+
+	for (c = 0, d = 0; c < (i + j + 1); c++)
 	{
-		string[k] = s2[j];
-		j++;
-		k++;
+		if (c < i)
+			a[c] = s1[c];
+		else
+			a[c] = s2[d++];
 	}
-	string[k] = '\0';
-	return (string);
+
+	return (a);
 }
