2d2c187cf67559bdba48b9eacef4489cd1ce6d4f
BrandynR <BrandynR@github.com>
2019-07-17 02:42:44 +0000

    Finished 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free up grid
+ * @grid: pointer to a pointer to grid
+ * @height: height
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
