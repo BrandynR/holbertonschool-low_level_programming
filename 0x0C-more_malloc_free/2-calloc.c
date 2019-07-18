7a533f62ab7d27ed039a8bbe3923b82a88dd5828
BrandynR <BrandynR@github.com>
2019-07-18 23:02:14 +0000

    Correct 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,15 +11,15 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int *p;
 	unsigned int i;
-	char *spc;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	spc = malloc(nmemb * size);
-	if (spc == NULL)
+	p = malloc(size * nmemb);
+	if (p == NULL)
 		return (NULL);
-	for (i = 0; i != size; i++)
-		*(spc + (size * i)) = 0;
-	return (spc);
+	for (i = 0; i < nmemb ; i++)
+		p[i] = 0;
+	return (p);
 }
