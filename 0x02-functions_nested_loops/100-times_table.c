d1586f34f391a91cc2c58c21ba823a3643d9097f
BrandynR <BrandynR@github.com>
2019-07-25 00:14:34 +0000

    Fixed task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,60 +1,18 @@
 #include "holberton.h"
 
 /**
- * print_times_table - prints times table for numbers from 0-14
+ * main - check the code for Holberton School students.
  *
- * @n: time table to print
- * Return: void
+ * Return: Always 0.
  */
-void print_times_table(int n)
+int main(void)
 {
-	int i, j;
-
-	i = 0;
-	if (n >= 0 && n < 15)
-	{
-		while (i <= n)
-		{
-			_putchar('0');
-			j = 1;
-			while (j <= n)
-				putformatnumber(i * j++);
-			_putchar('\n');
-			i++;
-		}
-	}
-}
-
-/**
- * putformatnumber - puts formatted characters to output
- *
- * @n: number to format
- * Return: void
- */
-void putformatnumber(int n)
-{
-	if (n < 10)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n >= 10 && n < 100)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
 }
