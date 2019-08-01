97b733ae988a2df348ac79d0e7d4437576680cb5
BrandynR <BrandynR@github.com>
2019-08-01 21:05:22 +0000

    Fix to get last check

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,11 +8,15 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	int i = 0;
 
-	for (i = 0; h; i++)
+	while (h)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str)
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		i++;
 		h = h->next;
 	}
 	return (i);
