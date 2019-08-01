d760d06327ed52e72a52d6a59463dc4b3bc41e73
BrandynR <BrandynR@github.com>
2019-08-01 22:08:34 +0000

    I hope it was this simple

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * premain - prints a message before main runs
+ *Return: void prints sentence
+ */
+void premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
