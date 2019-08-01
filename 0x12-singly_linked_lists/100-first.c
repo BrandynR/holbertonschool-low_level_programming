de846ea43a6d6f79c775ec2f8d2a969e9e2cadb5
BrandynR <BrandynR@github.com>
2019-08-01 22:11:32 +0000

    Added __attribute__

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * premain - prints a message before main runs
  *Return: void prints sentence
  */
-void premain(void)
+void premain(void)__attribute__((constructor))
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
