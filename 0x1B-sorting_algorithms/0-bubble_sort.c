c2138d5869ed218d220b10271e1024509a5c3670
BrandynR <BrandynR@github.com>
2019-10-30 16:34:32 +0000

    [LOCAL][ADD] BUbble sort alogorithm

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "sort.h"
 
 /**
@@ -29,4 +31,26 @@ void bubble_sort(int *array, size_t size)
 		if (flag == 0)
 			break;
 	}
+}
+
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
