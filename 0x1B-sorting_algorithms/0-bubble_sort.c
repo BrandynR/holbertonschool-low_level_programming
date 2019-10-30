19f1180907fb85044937eec2c92b58f9758f485f
BrandynR <BrandynR@github.com>
2019-10-30 21:37:26 +0000

    UPdated Bubble Sort

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,67 +1,28 @@
 #include "sort.h"
 
 /**
- * swap -  swap array elements
- * @ptr1: ptr to array element
- * @ptr2: prt to array element
- * Return: void
- */
-void swap(int *ptr1, int *ptr2)
-{
-	int temp = *ptr1;
-	*ptr1 = *ptr2;
-	*ptr2 = temp;
-}
-
-/**
- * sort_array - Returns if an array is sorted or not
- * @array: The array to check
- * @size: The size of the array
- *
- * Return: 1 if sorted, otherwise 0
- */
-int sort_array(int *array, size_t size)
-{
-	int loop = 0;
-
-	while (loop != (int)size)
-	{
-		if (array[loop - 1] > array[loop])
-			return (0);
-
-		loop++;
-	}
-
-	return (1);
-}
-
-/**
- * bubble_sort - sort an array of integers asending order
- * @array: input array
+ * bubble_sort - sorts array using bubble sort
+ * @array: array to be sorted
  * @size: size of array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	char isSorted;
+	unsigned int i, j, temp;
 
-	for (i = 1; i < size; i++)
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		while (sort_array(array, size) == 0)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			isSorted = 1;
-			for (j = 0; j < size - 1; j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					swap(&array[j], &array[j + 1]);
-					isSorted = 0;
-					print_array(array, size);
-				}
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
-		/* if no two elements were swapped by inner loop, then break */
-		if (isSorted == 1)
-			break;
 	}
 }
