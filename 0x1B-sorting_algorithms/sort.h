280cc759f50f809dcdc81d263bc4039071d25d5d
BrandynR <BrandynR@github.com>
2019-10-30 21:48:05 +0000

    Fixed typo in header

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -17,12 +17,6 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-typedef struct listint_s
-{
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_t;
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
