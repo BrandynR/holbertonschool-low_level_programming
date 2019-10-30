a85e8bd31bdbed3151851a0cbe65ce0be0583e1d
BrandynR <BrandynR@github.com>
2019-10-30 21:10:02 +0000

    Sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,114 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 
 /**
- * insertion_sort_list - insertion sorts a list
- * @list: head of list
+ * insertion_sort_list - sorting a double linked list with insertion
+ * @list: double linked list
+ *
+ * Return: void, returns nothing
  */
+
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp;
-	unsigned int i;
+	listint_t *element = NULL;
+	listint_t *tmp = NULL;
+	listint_t *head = NULL;
 
-	if (!list || !(*list))
+	if (!list || !(*list) || !(*list)->next)
 		return;
-	if (list_length(list) <= 1)
-		return;
-	for (i = 1; temp; i++)
+
+	head = *list;
+	while (head)
 	{
-		temp = from_index(list, i);
-		if (temp && temp->n < (temp->prev)->n)
+		element = head;
+		while (element->prev && element != NULL)
 		{
-			list_swap(list, i);
-			print_list(*list);
-			i = 0;
+			tmp = element->prev;
+			if (tmp->n > element->n)
+			{
+				if (tmp->prev)
+					(tmp->prev)->next = element;
+				element->prev = tmp->prev;
+				tmp->next = element->next;
+				tmp->prev = element;
+				element->next = tmp;
+				if (tmp->next)
+					(tmp->next)->prev = tmp;
+				if (!element->prev)
+					*list = element;
+				print_list(*list);
+			}
+			else
+				element = element->next;
+			if (!element)
+				return;
 		}
+		head = head->next;
 	}
-}
-
-/**
- * from_index - grabs a node at a specific index
- * @list: list to grab from
- * @index: index to grab at
- * Return: returns a pointer to the node specified
- */
-listint_t *from_index(listint_t **list, unsigned int index)
-{
-	listint_t *temp;
-	unsigned int i;
-
-	temp = *list;
-	for (i = 0; temp != NULL && i != index; i++)
-	{
-		temp = temp->next;
-	}
-	return (temp);
-}
-
-/**
- * list_swap - swaps a node with the previous node
- * @list: list to work from
- * @index: index to swap at
- * Return: returns 1 on success
- */
-unsigned int list_swap(listint_t **list, unsigned int index)
-{
-	listint_t *at_index, *temp;
-
-	at_index = from_index(list, index);
-	temp = at_index->prev;
-
-	temp->next = at_index->next;
-	at_index->prev = temp->prev;
-	temp->prev = at_index;
-	at_index->next = temp;
-	if (at_index->prev != NULL)
-		(at_index->prev)->next = at_index;
-	if (temp->next != NULL)
-		(temp->next)->prev = temp;
-	if (index == 1)
-	{
-		*list = at_index;
-	}
-	return (1);
-}
-
-/**
- * list_length - returns length of a linked list
- * @list: head of list
- * Return: Returns length of linked list
- */
-unsigned int list_length(listint_t **list)
-{
-	listint_t *temp;
-	unsigned int i;
-
-	temp = *list;
-
-	i = 0;
-	while (temp != NULL)
-	{
-		i++;
-		temp = temp->next;
-	}
-	return (i);
-}
-
-/**
- * rev_list - prints a list backwards, for testing
- * @list: head of list
- * Return: 1 on success
- */
-unsigned int rev_list(listint_t **list)
-{
-	listint_t *temp;
-
-	temp = *list;
-	while (temp->next != NULL)
-		temp = temp->next;
-	while (temp != NULL)
-	{
-		printf("%d, ", temp->n);
-		temp = temp->prev;
-	}
-	return (1);
 }
