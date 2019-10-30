a1f9baebf1ff874e9f0bba59140e9bf3a5205d48
BrandynR <BrandynR@github.com>
2019-10-30 22:03:14 +0000

    Updated header

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -25,4 +25,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void qs(int *array, size_t size, int start, int end);
+int partition(int *array, size_t size, int start, int pivot);
+void swap(int *array, size_t size, int i, int j);
+
 #endif
