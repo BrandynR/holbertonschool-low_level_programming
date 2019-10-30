28e8bbd7ea56126348837413673586216fa4d580
BrandynR <BrandynR@github.com>
2019-10-30 19:55:39 +0000

    Updated header

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -1,15 +1,12 @@
-#ifndef _SORT_H
-#define _SORT_H
+#ifndef HEADER_H
+#define HEADER_H
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 /**
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
@@ -19,71 +16,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-typedef enum kind_e
-{
-	SPADE = 0,
-	HEART,
-	CLUB,
-    DIAMOND
-} kind_t;
-
-/**
- * struct card_s - Playing card
- *
- * @value: Value of the card
- * From "Ace" to "King"
- * @kind: Kind of the card
- */
-typedef struct card_s
-{
-	const char *value;
-	const kind_t kind;
-} card_t;
-
-/**
- * struct deck_node_s - Deck of card
- *
- * @card: Pointer to the card of the node
- * @prev: Pointer to the previous node of the list
- * @next: Pointer to the next node of the list
- */
-typedef struct deck_node_s
-{
-	const card_t *card;
-	struct deck_node_s *prev;
-	struct deck_node_s *next;
-} deck_node_t;
-
-typedef struct listint_s
-{
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
-} listint_t;
-
-
-listint_t *create_listint(const int *array, size_t size);
-void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+void swap(int *ptr1, int *ptr2);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void shell_sort(int *array, size_t size);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-
-listint_t *from_index(listint_t **list, unsigned int index);
-unsigned int list_swap(listint_t **list, unsigned int index);
-unsigned int rev_list(listint_t **list);
-unsigned int list_length(listint_t **list);
-
-/* Quicksort Stuff */
-void qs(int *array, size_t size, int start, int end);
-int partition(int *array, size_t size, int pivot, int end);
-void swap(int *array, size_t size, int i, int j);
 #endif
