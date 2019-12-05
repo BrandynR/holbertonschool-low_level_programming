a9213f576fdfe89dd2eb136f28642e263363fd0c
BrandynR <BrandynR@github.com>
2019-12-05 06:07:50 +0000

    Counts the leaves in a binary tree

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -13,3 +13,16 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (1);
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
+
+/**
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to node being checked
+ * Return: 0 if node is not a leaf, 1 if node is
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node || node->right || node->left)
+		return (0);
+	return (1);
+}
