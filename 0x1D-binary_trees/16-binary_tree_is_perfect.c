9a2641279bf91b9c1e02312205420284c7bd329c
BrandynR <BrandynR@github.com>
2019-12-06 03:34:59 +0000

    Checks if binary tree is perfect

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -7,53 +7,23 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	if (binary_tree_is_full(tree) && binary_tree_balance(tree) == 0)
-		return (1);
-	return (0);
-}
-
-/**
- * binary_tree_is_full - check if a binary tree is full
- * @tree: pointer to the root node of the tree to check
- * Return: 1 on success, 0 if tree is NULL
- */
-
-int binary_tree_is_full(const binary_tree_t *tree)
-{
-	int left, right;
-
 	if (!tree)
 		return (0);
+	left = tree->left;
+	right = tree->right;
+	if (binary_tree_height(left) != binary_tree_height(right))
+		return (0);
 
-	if (!tree->left && !tree->right)
+	if (!left && !right)
 		return (1);
-	left = binary_tree_is_full(tree->left);
-	right = binary_tree_is_full(tree->right);
-	if (tree->left && tree->right)
-		return (left && right);
+
+	if (left && right)
+	{
+		if (binary_tree_is_perfect(left) && binary_tree_is_perfect(right))
+			return (1);
+	}
 
 	return (0);
-}
-
-/**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure the balance factor
- * Return: the balance factor of a binary tree, 0 if tree is NULL
- */
-
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	int leftsize = 0, rightsize = 0;
-
-	if (tree == NULL)
-		return (0);
-	if (tree->left)
-		leftsize = binary_tree_height(tree->left) + 1;
-	if (tree->right)
-		rightsize = binary_tree_height(tree->right) + 1;
-	return (leftsize - rightsize);
 }
 
 /**
