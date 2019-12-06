888c5cd55e97442558cee867ffa7cea829d135fa
BrandynR <BrandynR@github.com>
2019-12-06 22:24:14 +0000

    Typo fixed

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -26,7 +26,6 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
-typedef struct binary_tree_s b_t_t;
 
 /** Mandatory Task Prototypes */
 void binary_tree_print(const binary_tree_t *tree);
