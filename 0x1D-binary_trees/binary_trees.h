4071f89b00cfd7b3260fc301767f9378e15acc42
BrandynR <BrandynR@github.com>
2019-12-06 22:15:40 +0000

    Added typedef struct

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <sting.h>
+#include <string.h>
 #include <stddef.h>
 
 /**
@@ -23,11 +23,13 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+typedef struct binary_tree_s b_t_t;
 
 /** Mandatory Task Prototypes */
 void binary_tree_print(const binary_tree_t *tree);
-static size_t _height(const binary_tree_t *tree);
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
