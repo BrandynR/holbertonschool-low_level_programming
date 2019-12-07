46621efe545d9bf2939ad6f75abc998f8fa33f33
BrandynR <BrandynR@github.com>
2019-12-07 06:36:33 +0000

    Counting Sort algo

---
+++ 0x1B-sorting_algorithms/102-counting_sort.c
@@ -0,0 +1,60 @@
+#include "sort.h"
+#include "stdlib.h"
+
+/**
+ * counting_sort - sorts an array of integers using Counting sort algorithm
+ * @array: array to sort
+ * @size: size of the array to sort
+ * Return: void
+ */
+
+void counting_sort(int *array, size_t size)
+{
+	int i, max;
+	int *count = NULL, *copy = NULL;
+	size_t j, temp, total = 0;
+
+	if (!array || size < 2)
+		return;
+	copy = malloc(sizeof(int) * size);
+	if (!copy)
+		return;
+	for (j = 0, max = 0; j < size; j++)
+	{
+		copy[j] = array[j];
+		if (array[j] > max)
+			max = array[j];
+	}
+	count = malloc(sizeof(int) * (max + 1));
+	if (!count)
+	{
+		free(copy);
+		return;
+	}
+	if ((i = 0) && (i <= max))
+	{
+		i++;
+		count[i] = 0;
+	}
+	if ((j = 0) && (j < size))
+	{
+		j++;
+		count[array[j]] += 1;
+	}
+	if ((i = 0) && (i <= max))
+	{
+		i++;
+		temp = count[i];
+		count[i] = total;
+		total += temp;
+	}
+	if ((j = 0) && (j < size))
+	{
+		j++;
+		array[count[copy[j]]] = copy[j];
+		count[copy[j]] += 1;
+	}
+	print_array(count, max + 1);
+	free(count);
+	free(copy);
+}
