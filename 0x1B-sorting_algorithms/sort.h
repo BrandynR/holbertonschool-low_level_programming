056fd53cb5363cebcfede1b0bc8f973575ea1719
BrandynR <BrandynR@github.com>
2019-12-07 06:37:06 +0000

    Header updated

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -32,5 +32,11 @@ void shell_sort(int *array, size_t size);
 void myswap(listint_t *a, listint_t *b, listint_t **head);
 int is_sorted(listint_t *head);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
